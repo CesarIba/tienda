7a23276a340107233ad8d588f414ced18d6af314
CesarJIbanezV <cartod86@gmail.com>
2021-09-23 18:09:21 -0500

    Se actualiza estilo gestionUsuarios. Se integran funcionalidades CRUD usuarios

--- src/main/java/controller/servletGestionUsu.java
+++ src/main/java/controller/servletGestionUsu.java
@@ -1,6 +1,7 @@
 package controller;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -41,23 +42,93 @@ public class servletGestionUsu extends HttpServlet {
 		
 		String nombreUsu,mailUsu,usuario,claveUsu;
 		long cedulaUsu;
-		usuariosDTO usuDto;
+		usuariosDTO usuDto, usuDtoRes;
 		usuariosDAO usuDao = new usuariosDAO();
+
 		if(request.getParameter("btnInsertar")!=null) {
 
-			cedulaUsu=Long.parseLong(request.getParameter("cedula"));
-			nombreUsu=request.getParameter("nombre");
-			mailUsu=request.getParameter("email");
-			usuario=request.getParameter("usuario");
-			claveUsu=request.getParameter("clave");
+			if(!request.getParameter("cedula").isEmpty()) {
+				cedulaUsu=Long.parseLong(request.getParameter("cedula"));
+				nombreUsu=request.getParameter("nombre");
+				mailUsu=request.getParameter("email");
+				usuario=request.getParameter("usuario");
+				claveUsu=request.getParameter("clave");
+				
+				usuDto = new usuariosDTO(cedulaUsu, mailUsu, nombreUsu, claveUsu, usuario);
+				Boolean result = usuDao.insertar_usuario(usuDto);
+				if(result) {
+					JOptionPane.showMessageDialog(null, "Se agrego correctamente el usuario");
+					response.sendRedirect("index.jsp");
+				}else {
+					JOptionPane.showMessageDialog(null, "No se agrego el usuario");
+					response.sendRedirect("GestionUsuarios.jsp");
+				}
+			} else {
+				JOptionPane.showMessageDialog(null, "Ingrese numero de cedula");
+				response.sendRedirect("GestionUsuarios.jsp");
+			}
 			
-			usuDto = new usuariosDTO(cedulaUsu, mailUsu, nombreUsu, claveUsu, usuario);
-			Boolean result = usuDao.insertar_usuario(usuDto);
-			if(result) {
-				JOptionPane.showMessageDialog(null, "Se agrego correctamente el usuario");
-				response.sendRedirect("index.jsp");
-			}else {
-				JOptionPane.showMessageDialog(null, "No se agrego el usuario");
+		} else if(request.getParameter("btnConsultar")!=null) {
+
+			if(!request.getParameter("cedula").isEmpty()) {
+				cedulaUsu=Long.parseLong(request.getParameter("cedula"));
+				usuDto = new usuariosDTO(cedulaUsu, null, null, null, null);
+				usuDtoRes = usuDao.consultarId(usuDto);
+				if(usuDtoRes!=null) {
+					JOptionPane.showMessageDialog(null, "Se encontro al usuario");
+					cedulaUsu=usuDtoRes.getCedula_usuario();
+					nombreUsu=usuDtoRes.getNombre_usuario();
+					mailUsu=usuDtoRes.getMail_usuario();
+					usuario=usuDtoRes.getUsuario();
+					claveUsu=usuDtoRes.getClave();
+					response.sendRedirect("GestionUsuarios.jsp?ced="+cedulaUsu+"&&nom="+nombreUsu+"&&mail="+mailUsu+"&&usu="+usuario+"&&clv="+claveUsu);
+				}else {
+					JOptionPane.showMessageDialog(null, "No se encontró el usuario");
+					response.sendRedirect("GestionUsuarios.jsp");
+				}
+			} else {
+				JOptionPane.showMessageDialog(null, "Ingrese numero de cedula");
+				response.sendRedirect("GestionUsuarios.jsp");
+			}
+			
+		} else if(request.getParameter("btnEditar")!=null) {
+
+			int res;
+			if(!request.getParameter("cedula").isEmpty()) {
+				cedulaUsu=Long.parseLong(request.getParameter("cedula"));
+				nombreUsu=request.getParameter("nombre");
+				mailUsu=request.getParameter("email");
+				usuario=request.getParameter("usuario");
+				claveUsu=request.getParameter("clave");
+				usuDto = new usuariosDTO(cedulaUsu, mailUsu, nombreUsu, claveUsu, usuario);
+				res = usuDao.actualizarUsu(usuDto);
+				if(res>0) {
+					JOptionPane.showMessageDialog(null, "Se actualizo al usuario: " + cedulaUsu);
+					response.sendRedirect("GestionUsuarios.jsp");
+				} else {
+					JOptionPane.showMessageDialog(null, "No se encontró el usuario");
+					response.sendRedirect("GestionUsuarios.jsp");
+				}
+			} else {
+					JOptionPane.showMessageDialog(null, "Ingrese numero de cedula");
+					response.sendRedirect("GestionUsuarios.jsp");
+			}
+		} else if(request.getParameter("btnEliminar")!=null) {
+			int res;	
+			if(!request.getParameter("cedula").isEmpty()) {
+				cedulaUsu=Long.parseLong(request.getParameter("cedula"));
+				usuDto = new usuariosDTO(cedulaUsu, null, null, null, null);
+				res = usuDao.eliminarUsu(usuDto);
+				if(res>0) {
+					JOptionPane.showMessageDialog(null, "Se eliminó al usuario: " + cedulaUsu);
+					response.sendRedirect("GestionUsuarios.jsp");
+				}else {
+					JOptionPane.showMessageDialog(null, "No se encontró el usuario");
+					response.sendRedirect("GestionUsuarios.jsp");
+				}
+			} else {
+				JOptionPane.showMessageDialog(null, "Ingrese numero de cedula");
+				response.sendRedirect("GestionUsuarios.jsp");
 			}
 			
 		}
